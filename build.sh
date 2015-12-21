434f0d6a06c3c47c375fd4ca7241b6c7cd9a0c0e
Laurent Mathieu <laurent.mathieu@qcmedia.ca>
2015-12-21 14:41:14 -0500

    Ajout des scripts de build et push

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t dockistry.toumoro.com:5000/toumoro/php:5.4.43-apache .
---
+++ push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker push dockistry.toumoro.com:5000/toumoro/php:5.4.43-apache
