754bed6e8cde7531062cc8ec24df6f32a22d5d8c
Laurent Mathieu <laurent.mathieu@toumoro.com>
2021-06-07 13:45:24 -0400

    Added option to not change Apache UID

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-if [ ! -z "$HOST_UID" ];
+export PGU_BUILD=$(cat /var/www/version)
+
+if [ ! -z "$HOST_UID" ] & [ "$NO_UID_CHANGE" != true ]
 then
   usermod -u $HOST_UID www-data
   groupmod -g $HOST_UID www-data
-else
+elif [ -z "$HOST_UID" ] & [ "$NO_UID_CHANGE" != true ]
+then
   usermod -u 1000 www-data
   groupmod -g 1000 www-data
 fi
 
+# Lancement de Apache
 apache2-foreground
+
