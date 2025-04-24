cdc7bbcae842b69829b0b04cef204bc9d958452a
Laurent Mathieu <laurent.mathieu@toumoro.com>
2025-04-24 13:24:14 -0400

    Mise à jour des noms des variables d'environnement

--- configure.sh
+++ configure.sh
@@ -12,17 +12,17 @@ then
 fi
 
 # Add domains to hosts file and generate SSL certificates for them
-if [ -n "$TOU_DOMAINS_LIST" ]
+if [ -n "$TM_DOMAINS_LIST" ]
 then
-  for domain in $TOU_DOMAINS_LIST; do
+  for domain in $TM_DOMAINS_LIST; do
     echo "127.0.0.1 ${domain}" >> /etc/hosts
   done
   rm -f /etc/httpd/ssl/ssl.crt /etc/httpd/ssl/ssl.key
-  mkcert -cert-file /etc/httpd/ssl/ssl.crt -key-file /etc/httpd/ssl/ssl.key $TOU_DOMAINS_LIST
+  mkcert -cert-file /etc/httpd/ssl/ssl.crt -key-file /etc/httpd/ssl/ssl.key $TM_DOMAINS_LIST
 fi
 
 # Setup redis sessions
-#if [-n $"TOU_REDIS_HOST" ]
+#if [-n $"TM_REDIS_HOST" ]
   #echo session.save_handler = redis >> /usr/local/etc/php/php.ini
-  #echo session.save_path = "tcp://\${TYPO3_REDIS_HOST}:6379" >> /usr/local/etc/php/php.ini
+  #echo session.save_path = "tcp://\${TM_REDIS_HOST}:6379" >> /usr/local/etc/php/php.ini
 #then
