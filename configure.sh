6e47214914b54cbf9b40e6a947ecfd7d82549932
Laurent Mathieu <laurent.mathieu@toumoro.com>
2025-05-22 13:26:31 -0400

    Correction du if pour la configuration des sessions sur Redis

--- configure.sh
+++ configure.sh
@@ -23,6 +23,7 @@ fi
 
 # Setup redis sessions
 if [-n $"TM_REDIS_HOST" ]
+then
   echo session.save_handler = redis | tee -a /etc/php/8.2/apache2/php.ini | tee -a /etc/php/8.2/cli/php.ini
   echo session.save_path = "tcp://\${TM_REDIS_HOST}:6379" | tee -a /etc/php/8.2/apache2/php.ini | tee -a /etc/php/8.2/cli/php.ini
-then
+fi
